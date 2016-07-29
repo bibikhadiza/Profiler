0a4050237cafeeac3d252de1470dc8e82f59eb36
Derick Quartey <niikwartey1@gmail.com>
2016-07-29 14:29:49 -0400

    add class pseudocode

---
+++ lib/profiler/employer.rb
@@ -0,0 +1,19 @@
+class Employer
+  attr_reader :all_prospects, :all_profiles
+  
+  def initialize
+    @all_prospects = []
+    @all_profiles = []
+  end
+  
+  def save(prospect)
+    # add prospect to prospect array
+    self.all_prospects << prospect
+  end
+  
+  def get_profiles(prospect)
+    save(prospect)
+    prospect.profiles
+  end
+  
+end 
---
+++ lib/profiler/prospect.rb
@@ -0,0 +1,20 @@
+class Prospect
+  attr_reader :name, :email :profiles
+
+  
+  def initialize(name = nil, email)
+    @name = name
+    @email = email
+    @profiles = []
+  end
+  
+  def profiles
+    # pass email to fullcontact API
+
+
+    #create Profile instances out of API hash
+
+
+    # update prospects :profiles
+  end
+end
